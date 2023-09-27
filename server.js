7730311a705440987e3230f5fc49c5c2b9469bd7
Dilaxshan.$(IT21011634) <87793477+Dilaxshan@users.noreply.github.com>
2023-09-27 23:04:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ app.use(cors());
 app.use(express.json());
 
 // static files access
-// app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 //routes
-// app.use("/api/V1/portfolio",require("./routes/portfolioRoute"));
+app.use("/api/V1/portfolio",require("./routes/portfolioRoute"));
 
-// app.get('*', function(req,res){
-//     res.sendFile(path.join(__dirname,'./client/build/index.html'))
-// })
+app.get('*', function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'))
+})
 
 //port
 const PORT = process.env.PORT || 8080;
